3be7e7d6fa1eaa625df49318d6fa5e01ff4d65b5
asi304 <asi304@BLRTAX09.sapient.com>
2016-10-23 10:49:51 +0530

    Sample data for hive

--- src/main/java/com/luckyakhi/mapreducedemo/jobs/MaxTemperatureJob.java
+++ src/main/java/com/luckyakhi/mapreducedemo/jobs/MaxTemperatureJob.java
@@ -1,5 +1,6 @@
 package com.luckyakhi.mapreducedemo.jobs;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
@@ -9,8 +10,10 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 import com.luckyakhi.mapreducedemo.mappers.MaxTemperatureMapper;
+import com.luckyakhi.mapreducedemo.reducers.MaxTemperatureReducer;
 
 public class MaxTemperatureJob extends Configured implements Tool{
 	
@@ -26,9 +29,21 @@ public class MaxTemperatureJob extends Configured implements Tool{
 		maxTemperatureJob.setOutputValueClass(LongWritable.class);
 		maxTemperatureJob.setJobName("Max Temperature Job");
 		maxTemperatureJob.setMapperClass(MaxTemperatureMapper.class);
+		maxTemperatureJob.setReducerClass(MaxTemperatureReducer.class);
 		FileInputFormat.setInputPaths(maxTemperatureJob, new Path(args[0]));
 		FileOutputFormat.setOutputPath(maxTemperatureJob, new Path(args[1]));
 		return maxTemperatureJob.waitForCompletion(true)==true?0:1;
 	}
+	
+	 public static void main(String[] args) throws Exception {
+	        int result = ToolRunner.run(new Configuration(), new MaxTemperatureJob(), args);
+	        System.out.println("Result of max temperature job " + result);
+	        if (result == 0) {
+	            System.out.println("Result of  max temperature job Success");
+	        }
+	        else {
+	            System.out.println("Result of  max temperature Failure");
+	        }
+	    }
 
 }
