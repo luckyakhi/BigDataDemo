41e38c2c8e1742c0bcce8ca0f0eba1709dc95796
asi304 <asi304@BLRASI30475930.sapient.com>
2016-11-01 20:50:11 +0530

    MaxTemperatureJob tested with multiple files

--- src/main/java/com/luckyakhi/mapreducedemo/jobs/MaxTemperatureJob.java
+++ src/main/java/com/luckyakhi/mapreducedemo/jobs/MaxTemperatureJob.java
@@ -1,6 +1,5 @@
 package com.luckyakhi.mapreducedemo.jobs;
 
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -8,17 +7,15 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.Tool;
 
 import com.luckyakhi.mapreducedemo.mappers.MaxTemperatureMapper;
+import com.luckyakhi.mapreducedemo.reducers.MaxTemperatureReducer;
 
-public class MaxTemperatureJob extends Configured implements Tool{
+public class MaxTemperatureJob{
 	
-	
-	@Override
-	public int run(String[] args) throws Exception {
-		Job maxTemperatureJob = new Job(getConf());
-		maxTemperatureJob.setJarByClass(Job.class);
+	public static void main(String[] args) throws Exception {
+		Job maxTemperatureJob = new Job();
+		maxTemperatureJob.setJarByClass(MaxTemperatureJob.class);
 		maxTemperatureJob.setInputFormatClass(TextInputFormat.class);
 		maxTemperatureJob.setMapOutputKeyClass(Text.class);
 		maxTemperatureJob.setMapOutputValueClass(LongWritable.class);
@@ -26,9 +23,10 @@ public class MaxTemperatureJob extends Configured implements Tool{
 		maxTemperatureJob.setOutputValueClass(LongWritable.class);
 		maxTemperatureJob.setJobName("Max Temperature Job");
 		maxTemperatureJob.setMapperClass(MaxTemperatureMapper.class);
+		maxTemperatureJob.setReducerClass(MaxTemperatureReducer.class );
 		FileInputFormat.setInputPaths(maxTemperatureJob, new Path(args[0]));
 		FileOutputFormat.setOutputPath(maxTemperatureJob, new Path(args[1]));
-		return maxTemperatureJob.waitForCompletion(true)==true?0:1;
+		System.exit(maxTemperatureJob.waitForCompletion(true)==true?0:1);
 	}
 
 }
